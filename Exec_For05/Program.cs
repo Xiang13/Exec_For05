80c538a04a781a757543573545ad9168a388c21d
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 18:28:59 +0800

    加入專案檔案。

---
+++ Exec_For05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For05
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int start = 1;
+			int end = 5;
+
+			for (int i = start; i <= end; i++)
+			{
+				string plus = new string('+', end - i);
+				Console.Write(plus);
+
+				for (int j = start; j <= i; j++)
+				{
+					Console.Write(i);
+				}
+				Console.WriteLine();
+			}
+
+			
+		}
+	}
+}
